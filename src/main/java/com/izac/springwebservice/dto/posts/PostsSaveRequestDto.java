ffb24763e231dd77343cd23b436f3a208a043360
IzacNocTurne <sonth2000@gmail.com>
2020-02-09 23:19:58 +0900

    no message

--- src/main/java/com/izac/springwebservice/dto/posts/PostsSaveRequestDto.java
+++ src/main/java/com/izac/springwebservice/dto/posts/PostsSaveRequestDto.java
@@ -1,6 +1,7 @@
 package com.izac.springwebservice.dto.posts;
 
 import com.izac.springwebservice.domain.posts.Posts;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,4 +22,12 @@ public class PostsSaveRequestDto {
                 .author(author)
                 .build();
     }
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
 }
--- src/main/java/com/izac/springwebservice/web/WebRestController.java
+++ src/main/java/com/izac/springwebservice/web/WebRestController.java
@@ -1,7 +1,7 @@
 package com.izac.springwebservice.web;
 
-        import com.izac.springwebservice.domain.posts.PostsRepository;
         import com.izac.springwebservice.dto.posts.PostsSaveRequestDto;
+        import com.izac.springwebservice.service.PostsService;
         import lombok.AllArgsConstructor;
         import org.springframework.web.bind.annotation.GetMapping;
         import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +12,7 @@ package com.izac.springwebservice.web;
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -20,8 +20,8 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto){
-        postsRepository.save(dto.toEntity());
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
 
     }
 }
