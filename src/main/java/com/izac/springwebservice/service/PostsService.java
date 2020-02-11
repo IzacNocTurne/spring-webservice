1c58cd42cfc2e19b42e42d80867dd59b9a0119d7
IzacNocTurne <sonth2000@gmail.com>
2020-02-11 23:44:43 +0900

    no message

--- src/main/java/com/izac/springwebservice/service/PostsService.java
+++ src/main/java/com/izac/springwebservice/service/PostsService.java
@@ -1,11 +1,16 @@
 package com.izac.springwebservice.service;
 
 import com.izac.springwebservice.domain.posts.PostsRepository;
+import com.izac.springwebservice.dto.posts.PostsMainResponseDto;
 import com.izac.springwebservice.dto.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.stream.Collectors;
+
+import java.util.List;
+
 
 @AllArgsConstructor
 @Service
@@ -15,5 +20,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto){
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
